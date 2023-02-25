152f5099d39dc285e67444cc6fa7607d51e3e7f8
Dan Train <dan.train@red-badger.com>
2023-02-25 21:52:23 +0000

    Add open graph meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,6 +35,33 @@ class CustomDocument extends Document<CustomDocumentProps> {
           <meta name="theme-color" content="#ffffff" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://www.shufflealbums.com" />
+          <meta name="twitter:title" content="Shuffle Albums" />
+          <meta
+            name="twitter:description"
+            content="Shuffle your liked albums on Spotify"
+          />
+          <meta
+            name="twitter:image"
+            content="https://www.shufflealbums.com/manifest-icon-192.maskable.png"
+          />
+          <meta name="twitter:creator" content="@danhappysalad" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Shuffle Albums" />
+          <meta
+            property="og:description"
+            content="Shuffle your liked albums on Spotify"
+          />
+          <meta property="og:site_name" content="Shuffle Albums" />
+          <meta property="og:url" content="https://www.shufflealbums.com" />
+          <meta
+            property="og:image"
+            content="https://www.shufflealbums.com/apple-touch-icon.png"
+          />
+
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
