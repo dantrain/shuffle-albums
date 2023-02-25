5b82ea4a4fec581202b7218f5fb4b84a026c5cd9
Dan Train <dan.train@red-badger.com>
2023-02-25 22:55:44 +0000

    Update meta viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
-import { GlobalStyles } from "twin.macro";
 import Head from "next/head";
+import { GlobalStyles } from "twin.macro";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <title>Shuffle Albums</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+      />
     </Head>
     <GlobalStyles />
     <Component {...pageProps} />
